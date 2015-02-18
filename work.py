1c2cca07a39b1237db61b730f4aca83651e141cd
Oleh Babyak <olehb@microsoft.com>
2015-02-18 17:15:33 +0100

    refined program, added output for all tweets and tweet table

--- work.py
+++ work.py
@@ -1,6 +1,9 @@
 __author__ = 'babya_000'
 import twitter
 import re
+import json
+import sys
+from prettytable import PrettyTable
 
 with open("settings.ini") as f:
     lines = f.readlines()
@@ -13,21 +16,17 @@ auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,
                            CONSUMER_KEY, CONSUMER_SECRET)
 
 twitter_api = twitter.Twitter(auth=auth)
-#https://api.twitter.com/1.1/statuses/retweets/:id.json
-#twitter_api.statuses
-
-
 print twitter_api
 #
 WORLD_WOE_ID = 1
 US_WOE_ID = 23424977
 
-import json
-from prettytable import PrettyTable
-#
+if len(sys.argv) > 1:
+    q = sys.argv[1]
+    print "Searching " + q
+else:
+    q = raw_input("Write Your Twit=")
 
-q = raw_input("Write Your Twit=")
-count = 10
 i = 1
 Filelife = True
 while Filelife is True:
@@ -39,15 +38,16 @@ while Filelife is True:
         continue
     print i
 myfile = open('Test(' + str(i) + str(q) + ').dat', 'w+')
-
+tweets_file = open("tweets_bodies.txt", "w+")
+table_file = open("tweets_table.txt", "w+")
 
 # See https://dev.twitter.com/docs/api/1.1/get/search/tweets
-
-search_results = twitter_api.search.tweets(q=q, count=count)
+count = 10
+search_results = twitter_api.search.tweets(q=q, count=count) #, result_type="popular")
 
 statuses = search_results['statuses']
 
-for _ in range(10):
+for _ in range(50):
     print "Length of statuses", len(statuses)
     try:
         next_results = search_results['search_metadata']['next_results']
@@ -58,9 +58,9 @@ for _ in range(10):
 
     search_results = twitter_api.search.tweets(**kwargs)
     statuses += search_results['statuses']
-    print json.dumps(statuses[0], indent=1)
 
-
+print >> tweets_file, json.dumps(statuses, indent=1)
+print "Found " + `len(statuses)` + " tweet objects"
 # informace pro twitter, jak
 retweet_uniq_text = {}
 count = 0
@@ -68,20 +68,17 @@ retweets = []
 for status in statuses:
     count += 1
     key = status['text']
-    retweets_today = []
     if status.has_key('retweeted_status') and not retweet_uniq_text.has_key(key):
-        if "Sat Feb 14 " not in status['created_at']:
-            continue
+        if "Mon Feb 16 " not in status['created_at']:
+            pass
         retweet_uniq_text[key] = 1
-        retweets_today[count] += 1
         retweets.append([status['retweet_count'],
              status['retweeted_status']['user']['screen_name'],
              status['text'],
              status['id'],
-             status['created_at'],
-             retweets_today])
-status_texts = [ status['text']
-                 for status in statuses ]
+             status['created_at']])
+
+print "Found " + `len(retweets)` + " retweets"
 twitter_highest_retweets = {}
 # 5 first max retweets
 sorted_retweets = sorted(retweets, reverse=True)[:5]
@@ -99,7 +96,13 @@ for status in statuses:
         tweets_5max[index] = status
 
 for status in tweets_5max:
-    myfile.write(json.dumps(status, indent = 1))
+    if status:
+        myfile.write(json.dumps(status, indent = 1))
+
+if not len(statuses):
+    print "No tweets found"
+    exit(-1)
+
 myfile.write(json.dumps(statuses[0], indent=1))
 
 screen_names = [ user_mention['screen_name']
@@ -110,19 +113,23 @@ hashtags = [ hashtag['text']
              for status in statuses
                  for hashtag in status['entities']['hashtags'] ]
 # PrettyTalbe = z informace , top 5 twittu
-pt = PrettyTable(field_names=['Count', 'Screen Name', 'Text', 'Id', 'Time', 'Today'])
+pt = PrettyTable(field_names=['Count', 'Screen Name', 'Text', 'Id', 'Time'])
 [ pt.add_row(row) for row in sorted(retweets, reverse=True)[:5] ]
 pt.max_width['Text'] = 60
 pt.align= 'l'
-print pt
+print >>table_file, pt
+
+if True:
+    exit(0)
+
 world_trends = twitter_api.trends.place(_id=WORLD_WOE_ID)
 us_trends = twitter_api.trends.place(_id=US_WOE_ID)
 print json.dumps(world_trends, indent=1)
 print
 print json.dumps(us_trends, indent=1)
 
-
-#
+status_texts = [ status['text']
+                 for status in statuses ]
 words = [ w
           for t in status_texts
               for w in t.split() ]
